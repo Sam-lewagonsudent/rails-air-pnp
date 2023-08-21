2db093d231329dab652276ac4280fbe02c1492b6
samy bougdim <samy.bougdim@live.fr>
2023-08-21 15:33:50 +0200

    retour mid reservation

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,2 +1,9 @@
 class Reservation < ApplicationRecord
+  belong_to :pools
+  belong_to :users
+
+  validates :start_date, presence: true
+  validates :end_date, presence: true
+  validates :deposit, presence: true, default: false
+  validates :owner_acceptation, presence: true, default: false
 end
