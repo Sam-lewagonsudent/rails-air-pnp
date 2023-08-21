1eaa8c0c126e843d1b67f161becc347a30790d51
jeanporro <jporro40@gmail.com>
2023-08-21 15:29:19 +0200

    PoolModel

--- app/models/pool.rb
+++ app/models/pool.rb
@@ -1,2 +1,14 @@
 class Pool < ApplicationRecord
+  belongs_to :users
+  has_many :reservations
+  
+
+  validates price:, presence: true, numericality: { greater_than: 0 }
+  validates address:, presence: true, length: { maximum: 100 }
+  validates description:, presence: true, length: { maximum: 500 }
+  validates lenght:, prensence: true, numericality: { greater_than: 0 }
+  validates width:, presence: true, numericality: { greater_than: 0 }
+  validates depth:, presence: true, numericality: { greater_than: 0 }
+  validates shape:, presence: true, length: { maximum: 50 }
+  validates facilities:, presence: true, length: { maximum: 100 }
 end
