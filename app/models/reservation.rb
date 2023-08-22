40ad3b91684adfca41e15513aa3b828e7f4e5914
anthony lambert <lamberta.mailbox@gmail.com>
2023-08-22 12:35:02 +0200

    did seeds + modified validates presence true of booleans + did rails active_storage:install to add a migrate file

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -2,16 +2,15 @@ class Reservation < ApplicationRecord
   belongs_to :pool
   belongs_to :user
 
-  # validates :deposit, presence: true
-  # validates :owner_acceptation, presence: true
-  # validates :start_date, presence: true
-  # validates :end_date, presence: true
-  # # validates :start_date_before_end_date
+  validates :deposit, inclusion: [true, false]
+  validates :owner_acceptation, inclusion: [true, false]
+  validates :start_date, presence: true
+  validates :end_date, presence: true
+  validate :start_date_before_end_date
 
   private
 
   def start_date_before_end_date
     errors.add(:start_date, "must be before end date") if start_date && end_date && start_date >= end_date
   end
-
 end
