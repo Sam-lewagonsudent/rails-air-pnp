76ec77ed31b8e6403ba109836b16b10986b45111
samy bougdim <samy.bougdim@live.fr>
2023-08-22 15:56:25 +0200

    navbar et pool index

--- app/controllers/pools_controller.rb
+++ app/controllers/pools_controller.rb
@@ -1,5 +1,6 @@
 class PoolsController < ApplicationController
   before_action :set_pool, only: %i[show edit update destroy]
+  skip_before_action :authenticate_user!, only: [:index]
 
   def index
     @pools = Pool.all
