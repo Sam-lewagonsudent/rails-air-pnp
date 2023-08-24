04683b6b2c846813f4d89733d7096fa3ced59841
samy bougdim <samy.bougdim@live.fr>
2023-08-24 16:52:35 +0200

    gros bug sa mere

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,16 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
+    if user_signed_in?
+      @reservations = []
+      Pool.where(user_id: current_user.id).each do |pool|
+        Reservation.where(pool_id: pool.id).each do |reservation|
+          @reservations << reservation if pool.user_id == current_user.id && reservation.owner_acceptation == false
+        end
+      end
+        # @reservations.each do |reservation|
+        #   @reservations << reservation if reservation.owner_acceptation == false
+        # end
+    end
   end
 end
